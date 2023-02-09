edce29ea6bf78169aabaeac5b74b153c6e512c01
Zhi4a <sohuga5656@gmail.com>
2023-02-09 19:53:15 +0300

    1

--- NewsPaper/news/urls.py
+++ NewsPaper/news/urls.py
@@ -6,11 +6,11 @@ urlpatterns = [
    path('', PostList.as_view(), name='post_list'),
    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
 
-   path('news/create/', NewsCreate.as_view(), name='news_create'),
+   path('news/edit/', NewsCreate.as_view(), name='news_edit'),
    path('news/<int:pk>/update/', NewsUpdate.as_view(), name='news_update'),
    path('news/<int:pk>/delete/', NewsDelete.as_view(), name='news_delete'),
 
-   path('articles/create/', ArticlesCreate.as_view(), name='articles_create'),
+   path('articles/edit/', ArticlesCreate.as_view(), name='articles_edit'),
    path('articles/<int:pk>/update/', ArticlesUpdate.as_view(), name='articles_update'),
    path('articles/<int:pk>/delete/', ArticlesDelete.as_view(), name='articles_delete'),
 ]
