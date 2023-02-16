a7629b9084cf1fd2d796a25e1890f67147a4e13c
Zhi4a <sohuga5656@gmail.com>
2023-02-16 17:51:14 +0300

    1

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -21,9 +21,15 @@ class Author(models.Model):
             self.rating += comment.rating
         self.save()
 
+    def __str__(self):
+        return f'{self.user.username}'
+
 
 class Category(models.Model):
     category = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f'{self.category}'
 
 
 class Post(models.Model):
