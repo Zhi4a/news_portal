a36594838d21a91c558a64dd96df42e0cf2c67c0
Zhi4a <sohuga5656@gmail.com>
2023-02-22 16:57:59 +0300

    1

--- NewsPaper/news/signals.py
+++ NewsPaper/news/signals.py
@@ -1,17 +1,20 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
-from .models import Post, SubscribersCategory
+from .models import Post, SubscribersCategory, User
 
 
 @receiver(post_save, sender=Post)
 def notify_subscribers(sender, instance, created, **kwargs):
     subject = f'''Новая статья: "{instance.title}" в категории {instance.category}'''
-    subs = list(SubscribersCategory.objects.filter(category=instance.category).values('subscribers__email'))
+    subs = list(SubscribersCategory.objects.filter(category=instance.category).values('subscribers'))
+    emails = []
     for sub in subs:
+        emails.append(User.objects.filter(id=sub).values('email'))
+    for email in emails:
         send_mail(
             subject=subject,
             message=instance.text[:50],
-            recipient_list=[sub],
+            recipient_list=[email],
         )
     print(f'''Новый пост: "{instance.post.title}" в категории {instance.category.category}''')
