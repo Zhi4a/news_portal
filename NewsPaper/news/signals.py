64c474e5ec1ed3afb48b30004b93aed1b2cfd35a
Zhi4a <sohuga5656@gmail.com>
2023-02-24 16:43:14 +0300

    1

--- NewsPaper/news/signals.py
+++ NewsPaper/news/signals.py
@@ -6,15 +6,16 @@ from .models import Post, SubscribersCategory, User
 
 @receiver(post_save, sender=Post)
 def notify_subscribers(sender, instance, created, **kwargs):
-    subject = f'''Новая статья: "{instance.title}" в категории {instance.category}'''
-    subs = list(SubscribersCategory.objects.filter(category=instance.category).values('subscribers'))
-    emails = []
-    for sub in subs:
-        emails.append(User.objects.filter(id=sub).values('email'))
-    for email in emails:
-        send_mail(
-            subject=subject,
-            message=instance.text[:50],
-            recipient_list=[email],
-        )
-    print(f'''Новый пост: "{instance.post.title}" в категории {instance.category.category}''')
+    subject = f'''Новый пост: "{instance.title}" в категории {instance.category}'''
+    if created:
+        subs = list(SubscribersCategory.objects.filter(category=instance.category).values('subscribers'))
+        emails = []
+        for sub in subs:
+            emails.append(User.objects.filter(id=sub.id).values('email'))
+        for email in emails:
+            send_mail(
+                subject=subject,
+                message=instance.text[:50],
+                recipient_list=[email],
+            )
+        print(f'''Новый пост: "{instance.post.title}" в категории {instance.category.category}''')
